06822324004910e93a9c74870542b1fb42ad9054
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-10-12 12:06:50 +0300

    add signup and login template pages

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views 
 
 urlpatterns = [
     url(r'^accounts/', include('registration.backends.simple.urls')),
