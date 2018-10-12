1750e63f2b96d3215fa4fe136b616dbe013b14b1
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-10-12 12:20:49 +0300

    redirect to home page on login and add navbar and details

--- awards/settings.py
+++ awards/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
 ]
-
+LOGIN_REDIRECT_URL = 'home'
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
