3c274bace21748d409449c73ebb014630030329e
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-10-12 11:13:30 +0300

    add url for login and signup

--- awards/urls.py
+++ awards/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'', include('awwwards.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
